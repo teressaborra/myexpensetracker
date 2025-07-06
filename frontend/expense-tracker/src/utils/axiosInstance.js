6773563c2772670faea803ba002fc878db439827
Borra Teressa <teressa_csd@srkrec.edu.in>
2025-07-06 22:02:01 +0530

    Update axiosInstance.js

--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://myexpensetracker-541w.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
